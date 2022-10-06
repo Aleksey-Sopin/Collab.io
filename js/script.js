d9d02b8c00a3d298c347dccaedd332f945c15324
Aleksey-Sopin <sopinaleksey.sopin.kh@gmail.com>
2022-10-06 09:46:29 +0300

    removeCanvac

--- js/script.js
+++ js/script.js
@@ -24,37 +24,5 @@ $(document).ready(function(){
 });
 // =========
 
-let x = document.querySelector('.MyCanvas');
-
-let xctx = x.getContext('2d');
-xctx.font = '32px Aria';
-
-let gradient = xctx.createLinearGradient(0, 0, x.width, 50);
-gradient.addColorStop('0', 'yellow');
-gradient.addColorStop('1', 'blue');
-
-xctx.fillStyle = gradient;
-xctx.fillText('Слава Україні!', 13, 90);
-
-
-
-Object.assign(x.style, {
-	position: 'absolute',
-	top: '50%',
-	left: '40%',
-	display: 'none',
-	width: '300px',
-});
-
-let logoMain = document.querySelector('.header__logo');
-
-logoMain.addEventListener('click', function (event) {
-	x.style.display = 'inline';
-})
-
-x.addEventListener('click', function (event) {
-	x.style.display = 'none';
-})
-	
 
 
