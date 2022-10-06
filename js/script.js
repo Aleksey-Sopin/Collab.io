7bcaf5b1624ed88f14ce571614a028284f48b0b7
Aleksey-Sopin <sopinaleksey.sopin.kh@gmail.com>
2022-10-06 10:56:58 +0300

    add#

--- js/script.js
+++ js/script.js
@@ -23,6 +23,117 @@ $(document).ready(function(){
 	});
 });
 // =========
+let featuresLink = document.querySelector('#featureLink');
 
+featuresLink.addEventListener('click', function (event) {
 
+	if (window.matchMedia("(max-width: 767.98px)").matches) {
+		window.scrollTo({
+			top: 1400,
+			left: 0,
+			// smooth, instant,
+			// or auto; default
+			behavior: "smooth"
+		});
+	} else {
+		window.scrollTo({
+			top: 1250,
+			left: 0,
+			// smooth, instant,
+			// or auto; default
+			behavior: "smooth"
+		});
+	}
+})
+// =========
+let aboutLink = document.querySelector('#aboutLink');
 
+aboutLink.addEventListener('click', function (event) {
+
+	if (window.matchMedia("(max-width: 767.98px)").matches) {
+		window.scrollTo({
+			top: 2400,
+			left: 0,
+			// smooth, instant,
+			// or auto; default
+			behavior: "smooth"
+		});
+	} else {
+		window.scrollTo({
+			top: 2000,
+			left: 0,
+			// smooth, instant,
+			// or auto; default
+			behavior: "smooth"
+		});
+	}
+})
+// ===============
+let pricingLink = document.querySelector('#pricingLink');
+
+pricingLink.addEventListener('click', function (event) {
+
+	if (window.matchMedia("(max-width: 767.98px)").matches) {
+		window.scrollTo({
+			top: 8100,
+			left: 0,
+			// smooth, instant,
+			// or auto; default
+			behavior: "smooth"
+		});
+	} else {
+		window.scrollTo({
+			top: 5900,
+			left: 0,
+			// smooth, instant,
+			// or auto; default
+			behavior: "smooth"
+		});
+	}
+})
+// ===========
+let blogLink = document.querySelector('#blogLink');
+
+blogLink.addEventListener('click', function (event) {
+
+	if (window.matchMedia("(max-width: 767.98px)").matches) {
+		window.scrollTo({
+			top: 6980,
+			left: 0,
+			// smooth, instant,
+			// or auto; default
+			behavior: "smooth"
+		});
+	} else {
+		window.scrollTo({
+			top: 4950,
+			left: 0,
+			// smooth, instant,
+			// or auto; default
+			behavior: "smooth"
+		});
+	}
+})
+// ========================
+let contactLink = document.querySelector('#contactLink');
+
+contactLink.addEventListener('click', function (event) {
+
+	if (window.matchMedia("(max-width: 767.98px)").matches) {
+		window.scrollTo({
+			top: 10000,
+			left: 0,
+			// smooth, instant,
+			// or auto; default
+			behavior: "smooth"
+		});
+	} else {
+		window.scrollTo({
+			top: 9000,
+			left: 0,
+			// smooth, instant,
+			// or auto; default
+			behavior: "smooth"
+		});
+	}
+})
